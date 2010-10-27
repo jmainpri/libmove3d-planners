318046377949ff0a8f86b468fd41860b688ada23
Jim Mainprice <jmainpri@laas.fr>
2010-10-27 13:44:03 +0200

    Add glut window

---
+++ src/qtUI/glutWindow.hpp
@@ -0,0 +1,22 @@
+/*
+ *  glutWindow.hpp
+ *  OOMove3D
+ *
+ *  Created by Jim Mainprice on 25/10/10.
+ *  Copyright 2010 LAAS/CNRS. All rights reserved.
+ *
+ */
+
+#ifndef GLUT_DISPLAY_WIN_HPP
+#define GLUT_DISPLAY_WIN_HPP
+
+//! Class that displays a Glut window
+//! this class displays a Glut based window of the scene
+class GlutWindowDisplay 
+{
+public:
+	GlutWindowDisplay(int argc, char *argv[]);
+	
+};
+
+#endif
