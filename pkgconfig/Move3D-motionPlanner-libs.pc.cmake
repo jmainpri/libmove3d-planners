8fd1b340000970cca6abb8030a0fe9b3c6a01b25
JIM MAINPRICE <jmainpri@laas.fr>
2011-02-14 21:12:08 +0100

    work on pkgconfig support

---
+++ pkgconfig/Move3D-motionPlanner-libs.pc.cmake
@@ -0,0 +1,14 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@CMAKE_INSTALL_PREFIX@/lib
+includedir=@CMAKE_INSTALL_PREFIX@/include
+
+MPLibsFlags= @Move3DMPLIB_CompilationFlags@
+MPLibsIncludes=@Move3DMPLIB_Compilation_includes@
+MPLibsLibs=@Move3DMPLIB_Compilation_libs@
+ 
+Name: Move3D-motionPlanner-libs
+Description: Motion Planning Platform - Headless library
+Version: @BIOMOVE3D_VERSION@
+Libs: ${MPLibsFlags} -L${libdir} -lMove3D-motionPlanner-libs
+Cflags: -I${includedir}/Move3D-motionPlanner-libs/include ${MPLibsIncludes} ${MPLibsFlags} 
