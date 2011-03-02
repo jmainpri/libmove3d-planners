f5930b06b50c860deb0e4f099e442c35056d692b
Jim Mainprice <jmainpri@purpan.laas.fr>
2011-03-02 07:59:23 +0100

    Add the libmove3d-motionplanner pkgconfig

---
+++ pkgconfig/libmove3d-motionplanner.pc.cmake
@@ -0,0 +1,14 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@CMAKE_INSTALL_PREFIX@/lib
+includedir=@CMAKE_INSTALL_PREFIX@/include
+
+MPLibsFlags= @Move3DMPLIB_CompilationFlags@
+MPLibsIncludes=@Move3DMPLIB_Compilation_includes@
+MPLibsLibs=@Move3DMPLIB_Compilation_libs@
+ 
+Name: libmove3d-motionplanner
+Description: Motion Planning Platform - Headless library
+Version: @BIOMOVE3D_VERSION@
+Libs: ${MPLibsFlags} -L${libdir} -llibmove3d-motionplanner
+Cflags: -I${includedir}/libmove3d-motionplanner/src ${MPLibsIncludes} ${MPLibsFlags} 
