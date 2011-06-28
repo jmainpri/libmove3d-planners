0ae4fd05687e1df280b828d989ed7d7cdbe34757
Mamoun Gharbi <magharbi@laas.fr>
2011-06-28 11:23:25 +0200

    adding a function to draw robot n times

--- src/API/Graphic/drawCost.cpp
+++ src/API/Graphic/drawCost.cpp
@@ -229,7 +229,7 @@ void drawGauge(int number, double cost)
   glDisable(GL_BLEND);
 }
 
-
+void drawSlice();
 
 /**
  * @ingroup graphics
@@ -238,16 +238,19 @@ void drawGauge(int number, double cost)
 #ifdef HRI_COSTSPACE
 void g3d_draw_hrics()
 {
-
-
-
+    int OTPListSize = OTPList.size();
+//    if (OTPListSize > 0)
+//    {
+//        drawSlice();
+//    }
+//    return;
 
 	if( ENV.getBool(Env::enableHri) )
 	{
 		if( ENV.getBool(Env::HRIPlannerCS) && ENV.getBool(Env::drawTraj) )
 		{
 			//          printf("Draw 2d path\n");
-			dynamic_cast<HRICS::ConfigSpace*>(HRICS_MotionPLConfig)->draw2dPath();
+			//dynamic_cast<HRICS::ConfigSpace*>(HRICS_MotionPLConfig)->draw2dPath();
 		}
 
 		if( ENV.getBool(Env::HRIPlannerCS) && ENV.getBool(Env::drawOTPTraj) )
@@ -463,4 +466,44 @@ void computeConfigCostOnTraj(p3d_rob* rob,configPt q)
     std::cout << "Cost for " << r_Cost->getName() << " = " 
     << global_costSpace->cost(q_Cost) << std::endl;
   }
+}
+
+void drawSlice()
+{
+    cout << "Draw slice" << endl;
+    Robot* rob = global_Project->getActiveScene()->getRobotByNameContaining("PR2");
+    Robot* human = dynamic_cast<HRICS::Workspace*>(HRICS_MotionPL)->getHuman();
+    shared_ptr<Configuration> q_cur(human->getCurrentPos());
+//    getConfListSize() {return m_configList.size();}
+//            configPt getRobotConfigAt
+
+    if (dynamic_cast<HRICS::OTPMotionPl*>(HRICS_MotionPLConfig))
+    {
+        dynamic_cast<HRICS::OTPMotionPl*>(HRICS_MotionPLConfig)->getConfList();
+    }
+
+    G3D_Window *win;
+    win = g3d_get_cur_win();
+    win->vs.transparency_mode= G3D_TRANSPARENT_AND_OPAQUE;
+    win->vs.transparency_mode= G3D_OPAQUE;
+
+    for(int i = 0; i < dynamic_cast<HRICS::OTPMotionPl*>(HRICS_MotionPLConfig)->getConfListSize(); i++)
+    {
+//        shared_ptr<Configuration> q( new Configuration(rob,dynamic_cast<HRICS::OTPMotionPl*>(HRICS_MotionPLConfig)->getRobotConfigAt(i) ));
+//        rob->setAndUpdate(*q);
+        dynamic_cast<HRICS::OTPMotionPl*>(HRICS_MotionPLConfig)->setRobotsToConf(i,PlanEnv->getBool(PlanParam::env_isStanding));
+        global_Project->getActiveScene()->setActiveRobot("PR2_ROBOT");
+        //cout << "g3d_draw_robot" << endl;
+        //q->print();
+        g3d_draw_robot(rob->getRobotStruct()->num, win );
+
+//        shared_ptr<Configuration> q( new Configuration(human,dynamic_cast<HRICS::OTPMotionPl*>(HRICS_MotionPLConfig)->getRobotConfigAt(i) ));
+//        human->setAndUpdate(*q);
+//        global_Project->getActiveScene()->setActiveRobot("ACHILE_HUMAN1");
+//        //cout << "g3d_draw_robot" << endl;
+//        //q->print();
+//        g3d_draw_robot(human->getRobotStruct()->num, win);
+    }
+    human->setAndUpdate(*q_cur);
+    cout << "--------------------------" << endl;
 }
