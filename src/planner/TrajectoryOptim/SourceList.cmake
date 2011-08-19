0f05fdc1e71cf0f6eec80d5b3d4d8cb0fa1d8b00
Jim Mainpri <jim.mainprice@laas.fr>
2011-08-19 18:57:56 +0200

    Fix Compile and Install of Trajectory files

--- src/planner/TrajectoryOptim/SourceList.cmake
+++ src/planner/TrajectoryOptim/SourceList.cmake
@@ -5,6 +5,7 @@ BM3D_INC_DIR_PROCESS (${BM3D_MODULE_NAME})
 
 BM3D_SRC_SUBDIR_PROCESS(trajectoryOptim.cpp)
 
+include(${CMAKE_SOURCE_DIR}/${BM3D_MODULE_NAME}/Classic/SourceList.cmake)
 include(${CMAKE_SOURCE_DIR}/${BM3D_MODULE_NAME}/Chomp/SourceList.cmake)
 include(${CMAKE_SOURCE_DIR}/${BM3D_MODULE_NAME}/Stomp/SourceList.cmake)
 
