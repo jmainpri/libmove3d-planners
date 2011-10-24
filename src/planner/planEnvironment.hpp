1679160fd17f9eb0b083f2fb7472b0ded0977905
Mamoun Gharbi <mamoun.gharbi@laas.fr>
2011-10-24 14:47:30 +0200

    Reparing spaces in plannerEnvironment files

--- src/planner/planEnvironment.hpp
+++ src/planner/planEnvironment.hpp
@@ -21,6 +21,7 @@ bool Env_stopUser();
 #ifdef QT_LIBRARY
 class PlanParam : public QObject
 {
+
   Q_OBJECT;
   Q_ENUMS(boolParameter);
   Q_ENUMS(intParameter);
@@ -37,121 +38,123 @@ public:
 namespace PlanParam
 {
 #endif
-	enum boolParameter 
-	{
-		stopPlanner,
-		isMaxDisNeigh,
-		isWeightedChoice,
-		
-		// Smoothing stage
-		partialShortcut,
-		saveTrajCost,
-		trajCostRecompute,
-		withMaxIteration,
-		withGainLimit,
-		withTimeLimit,
-		withSmoothing,
-		withShortCut,
-		withDeformation,
-		withDescent,
-    trajOptimTestMultiGauss,
-    
-    // Stomp/Chomp
-    withCurrentTraj,
-    doReplanning,
-    useSelectedDuration,
-        
-        // Drawing (gl) stuff
-        drawColorConfig,
-        drawOccupVoxels,
-        drawSampledPoints,
-        drawStaticVoxels,
-        drawRandomMap,
-        drawBoundingVolumes,
-        
-        //Object Transfert point variable
-        env_humanGridDraw,
-        env_robotGridDraw,
-        env_isStanding,
-        env_drawRandomPoint,
-        env_drawDistGrid,
-        env_drawOnlyBest,
-        env_normalRand,
-        env_fusedGridRand,
-        env_useSlice,
-        env_useOrientedSlice,
-        env_useAllGrid,
-        env_drawSlice,
-        env_showHumanTraj,
-        env_useOldDude,
-        env_isInit,
-        env_showText
-	};
-	
-	enum intParameter 
-	{
-		tata,
-    nb_pointsOnTraj,
+        enum boolParameter
+        {
+            stopPlanner,
+            isMaxDisNeigh,
+            isWeightedChoice,
 
-		// Object TransfertPoint variable
-		env_maxIter,
-		env_nbRandomRotOnly,
-		env_nbSittingRotation,
-		env_totMaxIter,
+            // Smoothing stage
+            partialShortcut,
+            saveTrajCost,
+            trajCostRecompute,
+            withMaxIteration,
+            withGainLimit,
+            withTimeLimit,
+            withSmoothing,
+            withShortCut,
+            withDeformation,
+            withDescent,
+            trajOptimTestMultiGauss,
 
-		// grid cells
-		env_xToDraw,
-		env_yToDraw,
-		env_timeShow,
-		env_pow,
-		env_MOTP
+            // Stomp/Chomp
+            withCurrentTraj,
+            doReplanning,
+            useSelectedDuration,
 
-	};
-	
-	enum doubleParameter 
-	{
-		// Optimization Variables
-		optimTimeLimit,
-		MaxFactor,
-		MinStep,
-		costTraj,
-    distMinToDraw,
-    trajDuration,
-    trajOptimStdDev,
-    trajOptimSmoothWeight,
-    trajOptimObstacWeight,
-        // Object TransfertPoint variable
-        env_randomXMinLimit,
-        env_randomXMaxLimit,
-        env_randomYMinLimit,
-        env_randomYMaxLimit,
-        env_robotSpeed,
-        env_humanSpeed,
-        env_timeStamp,
-        env_psi,
-        env_delta,
-        env_ksi,
-        env_rho,
-        env_objectNessecity,
-        env_sittingOffset,
-        env_limitRot,
-        env_Cellsize,
-        env_timeLimitation,
-        env_sitTimeLimitation,
-        env_timeToDump
+            // Drawing (gl) stuff
+            drawColorConfig,
+            drawOccupVoxels,
+            drawSampledPoints,
+            drawStaticVoxels,
+            drawRandomMap,
+            drawBoundingVolumes,
 
-	};
-	
-	enum stringParameter 
-	{
-		titi
-	};
-	
-	enum vectorParameter 
-	{
-		tutu
-	};
-	
+            //Object Transfert point variable
+            env_humanGridDraw,
+            env_robotGridDraw,
+            env_isStanding,
+            env_drawRandomPoint,
+            env_drawDistGrid,
+            env_drawOnlyBest,
+            env_normalRand,
+            env_fusedGridRand,
+            env_useSlice,
+            env_useOrientedSlice,
+            env_useAllGrid,
+            env_drawSlice,
+            env_showHumanTraj,
+            env_useOldDude,
+            env_isInit,
+            env_showText,
+            env_realTime
+        };
+
+        enum intParameter
+        {
+            tata,
+            nb_pointsOnTraj,
+
+            // Object TransfertPoint variable
+            env_maxIter,
+            env_nbRandomRotOnly,
+            env_nbSittingRotation,
+            env_totMaxIter,
+
+            // grid cells
+            env_xToDraw,
+            env_yToDraw,
+            env_timeShow,
+            env_pow,
+            env_MOTP
+
+        };
+
+        enum doubleParameter
+        {
+            // Optimization Variables
+            optimTimeLimit,
+            MaxFactor,
+            MinStep,
+            costTraj,
+            distMinToDraw,
+            trajDuration,
+            trajOptimStdDev,
+            trajOptimSmoothWeight,
+            trajOptimObstacWeight,
+            // Object TransfertPoint variable
+            env_randomXMinLimit,
+            env_randomXMaxLimit,
+            env_randomYMinLimit,
+            env_randomYMaxLimit,
+            env_robotSpeed,
+            env_humanSpeed,
+            env_timeStamp,
+            env_psi,
+            env_delta,
+            env_ksi,
+            env_rho,
+            env_objectNessecity,
+            env_sittingOffset,
+            env_limitRot,
+            env_Cellsize,
+            env_timeLimitation,
+            env_sitTimeLimitation,
+            env_timeToDump
+
+        };
+
+        enum stringParameter
+        {
+                titi
+        };
+
+        enum vectorParameter
+        {
+                tutu
+        };
+
+
 };
 
 // Object that holds all parameters
