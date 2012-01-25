058fa6494c9aaef72cdcffa36a415c302c0652ba
JIM MAINPRICE <jmainpri@laas.fr>
2012-01-25 12:16:22 +0100

    Add cspace file to compute volume in the cspace and remove rrt_star file

---
+++ src/API/ConfigSpace/cspace.cpp
@@ -0,0 +1,122 @@
+#include "cspace.hpp"
+#include "localpath.hpp"
+//#include "riehl_localpaths.hpp" // PathSegments
+//#include "mechanical_cost_functions.hpp" // 2d costmap cost function
+//#include "mltrrt_cost_based_selection.hpp" // clearance cost
+#include "cost_space.hpp"
+#include <cmath>
+
+double CSpace::traj_cost(p3d_traj* traj)
+{
+    double cost(0.);
+    p3d_localpath *lp;
+    if (traj)
+    {
+        lp = traj->courbePt;
+        while (lp)
+        {
+            LocalPath path(m_robot, lp);
+            cost += this->lp_cost(path.getBegin(), path.getEnd());
+            lp = lp->next_lp;
+        }
+    }
+    return(cost);
+}
+
+CSpaceCostMap2D::CSpaceCostMap2D()
+{
+    m_connection_radius_flag = true;
+}
+
+CSpaceCostMap2D::~CSpaceCostMap2D()
+{
+}
+
+double CSpaceCostMap2D::q_cost(confPtr_t q)
+{
+    return(computeIntersectionWithGround(*q));
+}
+
+double CSpaceCostMap2D::lp_cost(confPtr_t q1, confPtr_t q2)
+{
+    LocalPath p(q1, q2);
+//    PathSegments segments(p.getParamMax(), m_cost_step);
+//    double cost(0.);
+//    double cost_q_prev(this->q_cost(q1));
+//    //printf("path length, step : %f, %f\n", p.getParamMax(), m_cost_step);
+//    for(unsigned i(1); i < segments.nbPoints(); ++i)
+//    {
+//        double cost_q_next(this->q_cost(p.configAtParam(segments[i])));
+//        //cost += std::max(cost_q_prev, cost_q_next) * segments.step();
+//        cost += std::max(cost_q_next - cost_q_prev, 0.0) + segments.step() * 0.01;
+//        //printf("cost: %f\n", cost);
+//        cost_q_prev = cost_q_next;
+//    }
+    return global_costSpace->cost(p);
+}
+
+double CSpaceCostMap2D::volume()
+{
+    assert(m_c_robot);
+    // this means that there is 1 joint + the unused(legacy) joint 0
+    assert(m_c_robot->njoints == 1);
+    assert(m_c_robot->joints[1]->type == P3D_PLAN);
+    return(fabs((m_c_robot->joints[1]->dof_data[0].vmax - m_c_robot->joints[1]->dof_data[0].vmin) * 
+                (m_c_robot->joints[1]->dof_data[1].vmax - m_c_robot->joints[1]->dof_data[1].vmin)));
+}
+
+double CSpaceCostMap2D::unit_sphere()
+{
+    return(2*M_PI);
+}
+
+unsigned CSpaceCostMap2D::dimension()
+{
+    return(2);
+}
+
+GenericCSpace::GenericCSpace(path_cost_mode mode) :
+    m_mode(mode)
+{
+    m_connection_radius_flag = false;
+}
+
+GenericCSpace::~GenericCSpace()
+{
+}
+
+double GenericCSpace::q_cost(confPtr_t q)
+{
+    return(global_costSpace->cost(*q));
+}
+
+double GenericCSpace::lp_cost(confPtr_t q1, confPtr_t q2)
+{
+    LocalPath p(q1, q2);
+//    PathSegments segments(p.getParamMax(), m_cost_step);
+//    double cost(0.);
+//    double cost_q_prev(this->q_cost(q1));
+//    switch(m_mode)
+//    {
+//    case integral:
+//    {
+//        for(unsigned i(1); i < segments.nbPoints(); ++i)
+//        {
+//            double cost_q_next(this->q_cost(p.configAtParam(segments[i])));
+//            cost += (cost_q_next + cost_q_prev) * 0.5 * segments.step();
+//            cost_q_prev = cost_q_next;
+//        }
+//    }
+//    case work:
+//    {
+//        for(unsigned i(1); i < segments.nbPoints(); ++i)
+//        {
+//            double cost_q_next(this->q_cost(p.configAtParam(segments[i])));
+//            cost += std::max(cost_q_next - cost_q_prev, 0.0) + 0.01 * segments.step();
+//            cost_q_prev = cost_q_next;
+//        }
+//    }
+//    default: {}
+//    }
+    return global_costSpace->cost(p);
+}
---
+++ src/API/ConfigSpace/cspace.hpp
@@ -0,0 +1,100 @@
+#ifndef CSPACE_INCLUDED_HPP
+#define CSPACE_INCLUDED_HPP
+
+#include "API/Device/robot.hpp"
+#include "API/ConfigSpace/configuration.hpp"
+#include "P3d-pkg.h"
+
+class CSpace
+{
+public:
+    // IMPORTANT : to properly initialize a CSpace object, the following must be done :
+    // create the CSpace object,
+    // then in no particular order :
+    // call the set_step function,
+    // call the set_robot function.
+    CSpace() : m_cost_step(0)
+    {};
+
+    virtual ~CSpace() {};
+
+    void set_cost_step(const double& cost_step) { m_cost_step = cost_step; }
+
+    void set_step(const double& step) { m_step = step; }
+
+    double get_step() { return(m_step); }
+
+    void set_robot(Robot* robot) { m_robot = robot; m_c_robot = robot->getRobotStruct(); }
+
+    Robot* get_robot() { return(m_robot); }
+
+    virtual double q_cost(confPtr_t q) = 0;
+
+    virtual double lp_cost(confPtr_t q1, confPtr_t q2) = 0;
+
+    virtual double traj_cost(p3d_traj* traj);
+    
+    bool get_connection_radius_flag() { return(m_connection_radius_flag); }
+
+    void set_connection_radius_flag(bool flag) { m_connection_radius_flag = flag; }
+
+    virtual double volume() = 0;
+
+    virtual double unit_sphere() = 0;
+
+    virtual unsigned dimension() = 0;
+
+protected:
+    double m_step;
+    double m_cost_step;
+    Robot* m_robot;
+    p3d_rob* m_c_robot;
+    bool m_connection_radius_flag;
+};
+
+class CSpaceCostMap2D : public CSpace
+{
+public:
+    CSpaceCostMap2D();
+
+    virtual ~CSpaceCostMap2D();
+
+    virtual double q_cost(confPtr_t q);
+
+    virtual double lp_cost(confPtr_t q1, confPtr_t q2);
+
+    // Compute the volume of the cspace :
+    // i.e. the area of the 2d costmap.
+    virtual double volume();
+
+    virtual double unit_sphere();
+
+    virtual unsigned dimension();
+};
+
+class GenericCSpace : public CSpace
+{
+public:
+    enum path_cost_mode { integral, work };
+
+    GenericCSpace(path_cost_mode mode);
+
+    virtual ~GenericCSpace();
+
+    virtual double q_cost(confPtr_t q);
+
+    virtual double lp_cost(confPtr_t q1, confPtr_t q2);
+
+    // Compute the volume of the cspace :
+    // the product of all dofs.
+    virtual double volume() { return(0.0); };
+
+    virtual double unit_sphere() { return(0.0); };
+
+    virtual unsigned dimension() { return(0); };
+
+private:
+    path_cost_mode m_mode;
+};
+
+#endif // CSPACE_INCLUDED_HPP
