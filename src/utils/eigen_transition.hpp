42f8ccc3ae4306f8c158763316390ab550e35d04
Jim Mainprice <mainprice@gmail.com>
2015-08-05 19:01:47 +0200

    Add eigen transition file

---
+++ src/utils/eigen_transition.hpp
@@ -0,0 +1,12 @@
+#ifndef EIGEN_TRANSITION_HPP
+#define EIGEN_TRANSITION_HPP
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+namespace Eigen {
+  typedef Eigen::Affine3d Transform3d;
+  typedef Eigen::Affine2d Transform2d;
+}
+
+#endif
